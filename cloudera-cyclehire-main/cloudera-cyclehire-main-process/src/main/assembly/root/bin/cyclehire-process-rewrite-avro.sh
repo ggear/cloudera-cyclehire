89b8af01abe3325ffb0bc13673e9a61b196e1d4f
ggear <graham.gear@gmail.com>
2014-08-19 22:09:27 +0100

    Resolve partition rewrite repitition

--- cloudera-cyclehire-main/cloudera-cyclehire-main-process/src/main/assembly/root/bin/cyclehire-process-rewrite-avro.sh
+++ cloudera-cyclehire-main/cloudera-cyclehire-main-process/src/main/assembly/root/bin/cyclehire-process-rewrite-avro.sh
@@ -16,6 +16,8 @@ TABLE_PARTITION_YEAR="$7"
 TABLE_PARTITION_MONTH="$8"
 export HIVE_AUX_JARS_PATH="$(echo -n $(ls -m $ROOT_DIR/lib/jar/dep/*.jar)|sed 's/, /:/g')"
 
+TABLE_LOCATION=$ROOT_DIR_HDFS_PROCESSED/cleansed/rewrite/avro/$TABLE_CODEC
+
 if [ "$TABLE_COMPRESS" = "false" ]; then
 	TABLE_CODEC="none"
 fi
@@ -23,7 +25,7 @@ fi
 PARTITION_YEARS=()
 PARTITION_MONTHS=()
 if [ -z "$TABLE_PARTITION_YEAR" ] || [ -z "$TABLE_PARTITION_MONTH" ]; then
-	for PATH_REWRITE in $(hadoop jar $HADOOP_LIB_SEARCH org.apache.solr.hadoop.HdfsFindTool -find /tmp/cyclehire/processed -name _REWRITE); do
+	for PATH_REWRITE in $(hadoop jar $HADOOP_LIB_SEARCH org.apache.solr.hadoop.HdfsFindTool -find $TABLE_LOCATION -name _REWRITE); do
    		PARTITION_YEARS+=("$(basename $(dirname $(dirname $PATH_REWRITE))|cut -c 6-|tr -d '\n')")
    		PARTITION_MONTHS+=("$(basename $(dirname $PATH_REWRITE)|cut -c 7-|tr -d '\n')")
 	done
@@ -45,7 +47,7 @@ for((i=0;i<${#PARTITION_YEARS[@]};i++)); do
 		--hiveconf "cyclehire.table.partition.year=${PARTITION_YEARS[$i]}" \
 		--hiveconf "cyclehire.table.partition.month=${PARTITION_MONTHS[$i]}" \
 		--hiveconf "cyclehire.table.modifier=cleansed_rewrite" \
-		--hiveconf "cyclehire.table.location=$ROOT_DIR_HDFS_PROCESSED/cleansed/rewrite/avro/$TABLE_CODEC" \
+		--hiveconf "cyclehire.table.location=$TABLE_LOCATION" \
 		$CMD_LINE_ARGUMENTS \
 		-f "$ROOT_DIR/lib/ddl/processed_rewrite_avro.ddl"
 done
