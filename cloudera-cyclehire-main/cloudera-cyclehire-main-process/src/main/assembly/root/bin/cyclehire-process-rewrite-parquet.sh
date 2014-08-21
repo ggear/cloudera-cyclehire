5355cb5c7d9a88e87b8f59374e875e91808cb94e
ggear <graham.gear@gmail.com>
2014-08-21 10:35:56 +0100

    Update rewrite scripts to specify dfs.blocksize

--- cloudera-cyclehire-main/cloudera-cyclehire-main-process/src/main/assembly/root/bin/cyclehire-process-rewrite-parquet.sh
+++ cloudera-cyclehire-main/cloudera-cyclehire-main-process/src/main/assembly/root/bin/cyclehire-process-rewrite-parquet.sh
@@ -8,12 +8,13 @@ set -x
 
 CMD_LINE_ARGUMENTS="$1"
 ROOT_DIR_HDFS_PROCESSED=${2:-"$ROOT_DIR_HDFS_PROCESSED"}
-TABLE_PARTITION_SPLIT=${3:-"14000000000"}
-TABLE_COMPRESS=${4:-"false"}
-TABLE_CODEC=${5:-"none"}
-TABLE_CODEC_CLASS=${6:-"SNAPPY"}
-TABLE_PARTITION_YEAR="$7"
-TABLE_PARTITION_MONTH="$8"
+TABLE_BLOCKSIZE=${3:-"1000000000"}
+TABLE_PARTITION_SPLIT=${4:-"19000000000"}
+TABLE_COMPRESS=${5:-"false"}
+TABLE_CODEC=${6:-"none"}
+TABLE_CODEC_CLASS=${7:-"SNAPPY"}
+TABLE_PARTITION_YEAR="$8"
+TABLE_PARTITION_MONTH="$9"
 export HIVE_AUX_JARS_PATH="$(echo -n $(ls -m $ROOT_DIR/lib/jar/dep/*.jar)|sed 's/, /:/g')"
 
 TABLE_LOCATION=$ROOT_DIR_HDFS_PROCESSED/cleansed/rewrite/parquet/$TABLE_CODEC
@@ -39,10 +40,11 @@ for((i=0;i<${#PARTITION_YEARS[@]};i++)); do
 	hive \
 		--hiveconf "hive.stats.autogather=false" \
 		--hiveconf "hive.exec.dynamic.partition.mode=nonstrict" \
+		--hiveconf "dfs.blocksize=$TABLE_BLOCKSIZE" \
+		--hiveconf "parquet.block.size=2147483647" \
 		--hiveconf "parquet.compression=$TABLE_CODEC_CLASS" \
 		--hiveconf "cyclehire.table.codec=$TABLE_CODEC" \
 		--hiveconf "mapreduce.input.fileinputformat.split.minsize=$TABLE_PARTITION_SPLIT" \
-		--hiveconf "mapreduce.input.fileinputformat.split.maxsize=$TABLE_PARTITION_SPLIT" \
 		--hiveconf "cyclehire.table.partition.year=${PARTITION_YEARS[$i]}" \
 		--hiveconf "cyclehire.table.partition.month=${PARTITION_MONTHS[$i]}" \
 		--hiveconf "cyclehire.table.modifier=cleansed_rewrite" \
--- cloudera-cyclehire-main/cloudera-cyclehire-main-process/src/main/assembly/root/bin/cyclehire-process-rewrite-sequence.sh
+++ cloudera-cyclehire-main/cloudera-cyclehire-main-process/src/main/assembly/root/bin/cyclehire-process-rewrite-sequence.sh
@@ -8,12 +8,13 @@ set -x
 
 CMD_LINE_ARGUMENTS="$1"
 ROOT_DIR_HDFS_PROCESSED=${2:-"$ROOT_DIR_HDFS_PROCESSED"}
-TABLE_PARTITION_SPLIT=${3:-"700000000"}
-TABLE_COMPRESS=${4:-"true"}
-TABLE_CODEC=${5:-"snappy"}
-TABLE_CODEC_CLASS=${6:-"org.apache.hadoop.io.compress.SnappyCodec"}
-TABLE_PARTITION_YEAR="$7"
-TABLE_PARTITION_MONTH="$8"
+TABLE_BLOCKSIZE=${3:-"134217728"}
+TABLE_PARTITION_SPLIT=${4:-"5000000000"}
+TABLE_COMPRESS=${5:-"true"}
+TABLE_CODEC=${6:-"snappy"}
+TABLE_CODEC_CLASS=${7:-"org.apache.hadoop.io.compress.SnappyCodec"}
+TABLE_PARTITION_YEAR="$8"
+TABLE_PARTITION_MONTH="$9"
 export HIVE_AUX_JARS_PATH="$(echo -n $(ls -m $ROOT_DIR/lib/jar/dep/*.jar)|sed 's/, /:/g')"
 
 TABLE_LOCATION=$ROOT_DIR_HDFS_PROCESSED/cleansed/rewrite/sequence/$TABLE_CODEC
@@ -38,12 +39,12 @@ for((i=0;i<${#PARTITION_YEARS[@]};i++)); do
 	hive \
 		--hiveconf "hive.stats.autogather=false" \
 		--hiveconf "hive.exec.dynamic.partition.mode=nonstrict" \
+		--hiveconf "dfs.blocksize=$TABLE_BLOCKSIZE" \
 		--hiveconf "hive.exec.compress.output=$TABLE_COMPRESS" \
 		--hiveconf "mapreduce.map.output.compress.codec=$TABLE_CODEC_CLASS" \
 		--hiveconf "mapreduce.output.fileoutputformat.compress.type=BLOCK" \
 		--hiveconf "cyclehire.table.codec=$TABLE_CODEC" \
 		--hiveconf "mapreduce.input.fileinputformat.split.minsize=$TABLE_PARTITION_SPLIT" \
-		--hiveconf "mapreduce.input.fileinputformat.split.maxsize=$TABLE_PARTITION_SPLIT" \
 		--hiveconf "cyclehire.table.partition.year=${PARTITION_YEARS[$i]}" \
 		--hiveconf "cyclehire.table.partition.month=${PARTITION_MONTHS[$i]}" \
 		--hiveconf "cyclehire.table.modifier=cleansed_rewrite" \
