1fdbf51c9a7205c1403d6ad320092abe52bfe660
ggear <graham.gear@gmail.com>
2014-09-25 16:52:20 +0100

    Resolve issue with resolving conf

--- cloudera-cyclehire-main/cloudera-cyclehire-main-assembly/src/main/assembly/root/bin/cyclehire-ingest.sh
+++ cloudera-cyclehire-main/cloudera-cyclehire-main-assembly/src/main/assembly/root/bin/cyclehire-ingest.sh
@@ -2,7 +2,7 @@
 
 export ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
 
-source $ROOT_DIR/bin/cyclehire-cyclehire.env
+source $ROOT_DIR/bin/cyclehire.env
 
 $ROOT_DIR/lib/ingress/bin/cyclehire-ingress.sh
 $ROOT_DIR/lib/process/bin/cyclehire-process.sh
