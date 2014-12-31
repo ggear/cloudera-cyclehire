e6574834cb83805251c8ca5b18ba36a97e62060c
ggear <graham.gear@gmail.com>
2014-12-31 12:44:09 +1100

    Remove _REWRITE markers since Hive 0.13 no longer flushes partition on INSERT OVERWRITE

--- cloudera-cyclehire-main/cloudera-cyclehire-main-process/src/main/assembly/root/bin/cyclehire-process-rewrite-parquet.sh
+++ cloudera-cyclehire-main/cloudera-cyclehire-main-process/src/main/assembly/root/bin/cyclehire-process-rewrite-parquet.sh
@@ -51,4 +51,5 @@ for((i=0;i<${#PARTITION_YEARS[@]};i++)); do
 		--hiveconf "cyclehire.table.location=$TABLE_LOCATION" \
 		$CMD_LINE_ARGUMENTS \
 		-f "$ROOT_DIR/lib/ddl/processed_rewrite_parquet.ddl"
+	hadoop fs -rm -f $TABLE_LOCATION/${PARTITION_YEARS[$i]}/${PARTITION_MONTHS[$i]}/_REWRITE
 done
