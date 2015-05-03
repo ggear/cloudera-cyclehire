2180a8d1bb735f5d3d04b6bf6191c3b066edf4f1
ggear <graham.gear@gmail.com>
2015-05-03 16:27:47 +1000

    Ensure default database is set for Hive/Impala

--- cloudera-cyclehire-main/cloudera-cyclehire-main-assembly/src/main/assembly/root/bin/cyclehire-shell-hive.sh
+++ cloudera-cyclehire-main/cloudera-cyclehire-main-assembly/src/main/assembly/root/bin/cyclehire-shell-hive.sh
@@ -4,4 +4,4 @@ export ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
 
 source $ROOT_DIR/bin/cyclehire.env
 
-beeline -u "jdbc:hive2://$HIVE_HIVESERVER2_HOST:$HIVE_HIVESERVER2_PORT/$NAME_SPACE;user=$USER_ADMIN" -n $USER_ADMIN "$@"
+beeline -u "jdbc:hive2://$HIVE_HIVESERVER2_HOST:$HIVE_HIVESERVER2_PORT/$NAME_SPACE_DATABASE;user=$USER_ADMIN" -n $USER_ADMIN "$@"
